7b57bb353118a25441a08822e7b55b7688d88498
ichinogroup <dttlinh0210@gmail.com>
2026-05-22 17:35:14 +0900

    Remove electrical equipment PDF guide links
    Removed PDF guide links for electrical equipment in both English and Chinese sections.

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ const data = {
       ["Wifi", "ID: TP-Link_1F24_5G<br>PW: 59919995"],
 
       ["Before Entering The House","Please remove your shoes before entering."],
-
-      ["Electrical Equipment", `<a href="assets/pdf/shizuku-switch-guide.pdf" target="_blank">Open PDF Guide</a>`],
       ["Air Conditioner Remote", `<a href="assets/pdf/shizuku-airconditioner-remote.pdf" target="_blank">Open PDF Guide</a>`],
       ["Hot Water Panel", "There is only hot water control panels in the house, mounted on the wall behind the washing machine.<br><br><b>Important notes:</b><br><br>◆ If the temperature is accidentally set to <b>60°C</b>, water may stop flowing properly.<br>◆ If the water is not getting warm, please check whether the unit is turned <b>ON</b>.<br>◆ The system may turn itself off automatically after a period of inactivity.<br><br><img src='assets/images/shizuku-hot-water-panel.png' style='width:100%; max-width:320px; border-radius:10px;'>"],
       ["Trash Disposal", "Please separate domestic waste and cans/bottles into labeled bins.<br>If the bin is full, remove the garbage from the bin, place it outside the front door, and contact the staff for collection.<br><br><img src='assets/images/garbage.png' style='width:100%; max-width:320px; border-radius:10px;'>"],
@@ -137,7 +135,6 @@ const data = {
         `进入房间前请脱鞋。`
       ],
 
-      ["电器设备", `<a href="assets/pdf/切换指南.pdf" target="_blank">Open PDF Guide</a>`],
       ["空调调节",`<a href="assets/pdf/雫空调.pdf" target="_blank">Open PDF Guide</a>` ],
       ["热水温度", "屋内有一个热水控制面板，安装在洗衣机后面的墙上。<br><br><b>请注意：</b><br><br>◆ 如果误设为 <b>60℃</b>，热水可能无法正常流出，请将温度调低。<br>◆ 如果水不够热或没有变热，请确认设备是否已开启（<b>ON</b>）。<br>◆ 若一段时间未使用，设备可能会自动关闭。<br><br><img src='assets/images/shizuku-hot-water-panel.png' style='width:100%; max-width:320px; border-radius:10px;'>"],
       ["如何丢垃圾", "请将可燃垃圾、罐子/瓶子分类放入有标签的垃圾桶。如果垃圾桶满了，请将垃圾取出，放在房子前门外面，并联系工作人员前来收取。<br><br><img src='assets/images/garbage.png' style='width:100%; max-width:320px; border-radius:10px;'>"],
